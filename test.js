68d5cb2bceaa30bbf8e3385400fc4c497aec87ac
Fernando Machado Bernardino <fernandombernardino@gmail.com>
2019-07-24 00:12:26 -0300

    main test suites

--- test.js
+++ test.js
@@ -1,87 +1,25 @@
-'use strict';
+const CamelCaseSuite = require('./test-camel-case');
+const DashCaseSuite = require('./test-dash-case');
+const PascalCaseSuite = require('./test-pascal-case');
+const SnakeCaseSuite = require('./test-snake-case');
+const UpperDashCaseSuite = require('./test-upper-dash-case');
+const UpperSnakeCaseSuite = require('./test-upper-snake-case');
 
-var caseparser = require('./lib/caseparser');
 
-var snakeClients = [
-  {
-    id: 1,
-    is_under_age: false,
-    first_name: 'Timothee',
-    last_name: 'Clausner',
-    email: 'tclausner0@economist.com',
-    telephone_numbers: [
-      '(9999) 99999-9999',
-      '(8888) 88888-8888'
-    ],
-    addresses: [
-      {
-        country: 'United States',
-        state: 'Illinois',
-        city: 'Rockford',
-        postal_code: '61105',
-        street: {
-          street_name: '41 Forest Run Circle',
-          street_number: '539'
-        }
-      },
-      {
-        country: 'United States',
-        state: 'California',
-        city: 'Inglewood',
-        postal_code: '90305',
-        street: {
-          street_name: '456 Brown Center',
-          street_number: '550'
-        }
-      }
-    ]
-  }
-];
+// console.log('CamelCaseSuite:');
+// CamelCaseSuite.testCamelCaseSuite();
 
-console.log('======================================================');
-console.log('     Convert from \'snake_case\' to \'camelCase\'     ');
-console.log('------------------------------------------------------');
-console.log(JSON.stringify(caseparser.snakeToCamel(snakeClients), null, 2));
-console.log('------------------------------------------------------');
+console.log('DashCaseSuite:');
+DashCaseSuite.testDashCaseSuite();
 
-var camelClients = [
-  {
-    id: 1,
-    isUnderAge: false,
-    firstName: 'Timothee',
-    lastName: 'Clausner',
-    email: 'tclausner0@economist.com',
-    telephoneNumbers: [
-      '(9999) 99999-9999',
-      '(8888) 88888-8888'
-    ],
-    addresses: [
-      {
-        country: 'United_States',
-        state: 'Illinois',
-        city: 'Rockford',
-        postalCode: '61105',
-        street: {
-          streetName: '41 Forest Run Circle',
-          streetNumber: '539'
-        }
-      },
-      {
-        country: 'United States',
-        state: 'California',
-        city: 'Inglewood',
-        postalCode: '90305',
-        street: {
-          streetName: '456 Brown Center',
-          streetNumber: '550'
-        }
-      }
-    ]
-  }
-];
+// console.log('PascalCaseSuite:');
+// PascalCaseSuite.testPascalCaseSuite();
 
-console.log('======================================================');
-console.log('     Convert from \'camelCase\' to \'snake_case\'     ');
-console.log('------------------------------------------------------');
-console.log(JSON.stringify(caseparser.camelToSnake(camelClients), null, 2));
-console.log('------------------------------------------------------');
+// console.log('SnakeCaseSuite:');
+// SnakeCaseSuite.testSnakeCaseSuite();
+
+// console.log('UpperDashCaseSuite:');
+// UpperDashCaseSuite.testUpperDashCaseSuite();
+
+// console.log('UpperSnakeCaseSuite:');
+// UpperSnakeCaseSuite.testUpperSnakeCaseSuite();
