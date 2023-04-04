c348f222080ea63ddcb35575133efe4b82d2aafa
Fernando Machado Bernardino <fernandombernardino@gmail.com>
2023-04-04 16:47:48 -0300

    upperDashCase

---
+++ src/upperDashCase/upperDashCase.spec.ts
@@ -0,0 +1,243 @@
+import { describe, expect, test } from 'vitest';
+import caseparser from '../index';
+
+
+describe('UPPER-DASH-CASE', () => {
+  const input = {
+    string: 'GRAND-RAPIDS-CHARTER-TOWNSHIP',
+    json: {
+      "ID": 1,
+      "IS-UNDER-AGE": false,
+      "FIRST-NAME": "John",
+      "LAST-NAME": "Doe",
+      "EMAIL": "john.doe@example.com",
+      "TELEPHONE-NUMBERS": [
+        "(616) 361-1338",
+        "(907) 742-5450",
+      ],
+      "ADDRESSES": [
+        {
+          "COUNTRY": "United States",
+          "STATE": "Illinois",
+          "CITY": "Rockford",
+          "POSTAL-CODE": "61105",
+          "STREET": {
+            "STREET-NAME": "41 Forest Run Circle",
+            "STREET-NUMBER": "539"
+          }
+        },
+        {
+          "COUNTRY": "United States",
+          "STATE": "Texas",
+          "CITY": "Conroe",
+          "POSTAL-CODE": "77301",
+          "STREET": {
+            "STREET-NAME": "E Phillips St",
+            "STREET-NUMBER": "200"
+          }
+        }
+      ]
+    }
+  };
+  test('Should convert a string from upperDashToCamel', () => {
+    expect(caseparser.upperDashToCamel(input.string)).toMatchInlineSnapshot('"grandRapidsCharterTownship"');
+  });
+  test('Should convert a string from upperDashToDash', () => {
+    expect(caseparser.upperDashToDash(input.string)).toMatchInlineSnapshot('"grand-rapids-charter-township"');
+  });
+  test('Should convert a string from upperDashToPascal', () => {
+    expect(caseparser.upperDashToPascal(input.string)).toMatchInlineSnapshot('"GrandRapidsCharterTownship"');
+  });
+  test('Should convert a string from upperDashToSnake', () => {
+    expect(caseparser.upperDashToSnake(input.string)).toMatchInlineSnapshot('"grand_rapids_charter_township"');
+  });
+  test('Should convert a string from upperDashToUpperSnake', () => {
+    expect(caseparser.upperDashToUpperSnake(input.string)).toMatchInlineSnapshot('"GRAND_RAPIDS_CHARTER_TOWNSHIP"');
+  });
+  test('Should convert a json from upperDashToCamel', () => {
+    expect(caseparser.upperDashToCamel(input.json)).toMatchInlineSnapshot(`
+      {
+        "addresses": [
+          {
+            "city": "Rockford",
+            "country": "United States",
+            "postalCode": "61105",
+            "state": "Illinois",
+            "street": {
+              "streetName": "41 Forest Run Circle",
+              "streetNumber": "539",
+            },
+          },
+          {
+            "city": "Conroe",
+            "country": "United States",
+            "postalCode": "77301",
+            "state": "Texas",
+            "street": {
+              "streetName": "E Phillips St",
+              "streetNumber": "200",
+            },
+          },
+        ],
+        "email": "john.doe@example.com",
+        "firstName": "John",
+        "id": 1,
+        "isUnderAge": false,
+        "lastName": "Doe",
+        "telephoneNumbers": [
+          "(616) 361-1338",
+          "(907) 742-5450",
+        ],
+      }
+    `);
+  });
+  test('Should convert a json from upperDashToDash', () => {
+    expect(caseparser.upperDashToDash(input.json)).toMatchInlineSnapshot(`
+      {
+        "addresses": [
+          {
+            "city": "Rockford",
+            "country": "United States",
+            "postal-code": "61105",
+            "state": "Illinois",
+            "street": {
+              "street-name": "41 Forest Run Circle",
+              "street-number": "539",
+            },
+          },
+          {
+            "city": "Conroe",
+            "country": "United States",
+            "postal-code": "77301",
+            "state": "Texas",
+            "street": {
+              "street-name": "E Phillips St",
+              "street-number": "200",
+            },
+          },
+        ],
+        "email": "john.doe@example.com",
+        "first-name": "John",
+        "id": 1,
+        "is-under-age": false,
+        "last-name": "Doe",
+        "telephone-numbers": [
+          "(616) 361-1338",
+          "(907) 742-5450",
+        ],
+      }
+    `);
+  });
+  test('Should convert a json from upperDashToPascal', () => {
+    expect(caseparser.upperDashToPascal(input.json)).toMatchInlineSnapshot(`
+      {
+        "Addresses": [
+          {
+            "City": "Rockford",
+            "Country": "United States",
+            "PostalCode": "61105",
+            "State": "Illinois",
+            "Street": {
+              "StreetName": "41 Forest Run Circle",
+              "StreetNumber": "539",
+            },
+          },
+          {
+            "City": "Conroe",
+            "Country": "United States",
+            "PostalCode": "77301",
+            "State": "Texas",
+            "Street": {
+              "StreetName": "E Phillips St",
+              "StreetNumber": "200",
+            },
+          },
+        ],
+        "Email": "john.doe@example.com",
+        "FirstName": "John",
+        "Id": 1,
+        "IsUnderAge": false,
+        "LastName": "Doe",
+        "TelephoneNumbers": [
+          "(616) 361-1338",
+          "(907) 742-5450",
+        ],
+      }
+    `);
+  });
+  test('Should convert a json from upperDashToSnake', () => {
+    expect(caseparser.upperDashToSnake(input.json)).toMatchInlineSnapshot(`
+      {
+        "addresses": [
+          {
+            "city": "Rockford",
+            "country": "United States",
+            "postal_code": "61105",
+            "state": "Illinois",
+            "street": {
+              "street_name": "41 Forest Run Circle",
+              "street_number": "539",
+            },
+          },
+          {
+            "city": "Conroe",
+            "country": "United States",
+            "postal_code": "77301",
+            "state": "Texas",
+            "street": {
+              "street_name": "E Phillips St",
+              "street_number": "200",
+            },
+          },
+        ],
+        "email": "john.doe@example.com",
+        "first_name": "John",
+        "id": 1,
+        "is_under_age": false,
+        "last_name": "Doe",
+        "telephone_numbers": [
+          "(616) 361-1338",
+          "(907) 742-5450",
+        ],
+      }
+    `);
+  });
+  test('Should convert a json from upperDashToUpperSnake', () => {
+    expect(caseparser.upperDashToUpperSnake(input.json)).toMatchInlineSnapshot(`
+      {
+        "ADDRESSES": [
+          {
+            "CITY": "Rockford",
+            "COUNTRY": "United States",
+            "POSTAL_CODE": "61105",
+            "STATE": "Illinois",
+            "STREET": {
+              "STREET_NAME": "41 Forest Run Circle",
+              "STREET_NUMBER": "539",
+            },
+          },
+          {
+            "CITY": "Conroe",
+            "COUNTRY": "United States",
+            "POSTAL_CODE": "77301",
+            "STATE": "Texas",
+            "STREET": {
+              "STREET_NAME": "E Phillips St",
+              "STREET_NUMBER": "200",
+            },
+          },
+        ],
+        "EMAIL": "john.doe@example.com",
+        "FIRST_NAME": "John",
+        "ID": 1,
+        "IS_UNDER_AGE": false,
+        "LAST_NAME": "Doe",
+        "TELEPHONE_NUMBERS": [
+          "(616) 361-1338",
+          "(907) 742-5450",
+        ],
+      }
+    `);
+  });
+});
+
