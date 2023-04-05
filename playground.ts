b82f5cf5a66abef0fb8fce5ea6eaee99047538ab
Fernando Machado Bernardino <fernandombernardino@gmail.com>
2023-04-05 00:05:08 -0300

    removing files

--- playground.ts
+++
@@ -1,9 +0,0 @@
-import caseparser from './src';
-
-const stringTest = caseparser.camelToDash('loremIpsumIsSimplyDummyTextOfThePrintingAndTypesettingIndustry');
-//    ^?
-
-const jsonTest = caseparser.camelToDash({ firstName: 'John', lastName: 'Doe' });
-//    ^?
-
-
