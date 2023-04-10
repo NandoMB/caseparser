fa0459f0805ec8bec91eaeefc8538856499a2a2c
Fernando Machado Bernardino <fernandombernardino@gmail.com>
2023-04-09 22:13:05 -0300

    Adding typesafe

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,3 @@
-export type Prettify<T> = {
-  [K in keyof T]: T[K];
-} & {};
-
 type CamelToDash<T extends string> = T extends `${infer L}${infer R}` ? `${L extends Capitalize<L> ? '-' : ''}${Lowercase<L>}${CamelToDash<R>}` : T;
 type CamelToPascal<T extends string> = Capitalize<T>;
 type CamelToSnake<T extends string> = T extends `${infer L}${infer R}` ? `${L extends Capitalize<L> ? '_' : ''}${Lowercase<L>}${CamelToSnake<R>}` : T;
@@ -46,38 +42,49 @@ type ParserUpperDashType = 'UpperDashToCamel' | 'UpperDashToDash' | 'UpperDashTo
 type ParserUpperSnakeType = 'UpperSnakeToCamel' | 'UpperSnakeToDash' | 'UpperSnakeToPascal' | 'UpperSnakeToSnake' | 'UpperSnakeToUpperDash';
 export type ParserType = ParserCamelType | ParserDashType | ParserPascalType | ParserSnakeType | ParserUpperDashType | ParserUpperSnakeType;
 
-export type OutputType<T, P> = Prettify<{
-  [K in Extract<keyof T, string> as
-    P extends 'CamelToDash' ? CamelToDash<K> :
-    P extends 'CamelToPascal' ? CamelToPascal<K> :
-    P extends 'CamelToSnake' ? CamelToSnake<K> :
-    P extends 'CamelToUpperDash' ? CamelToUpperDash<K> :
-    P extends 'CamelToUpperSnake' ? CamelToUpperSnake<K> :
-    P extends 'DashToCamel' ? DashToCamel<K> :
-    P extends 'DashToPascal' ? DashToPascal<K> :
-    P extends 'DashToSnake' ? DashToSnake<K> :
-    P extends 'DashToUpperDash' ? DashToUpperDash<K> :
-    P extends 'DashToUpperSnake' ? DashToUpperSnake<K> :
-    P extends 'PascalToCamel' ? PascalToCamel<K> :
-    P extends 'PascalToDash' ? PascalToDash<K> :
-    P extends 'PascalToSnake' ? PascalToSnake<K> :
-    P extends 'PascalToUpperDash' ? PascalToUpperDash<K> :
-    P extends 'PascalToUpperSnake' ? PascalToUpperSnake<K> :
-    P extends 'SnakeToCamel' ? SnakeToCamel<K> :
-    P extends 'SnakeToDash' ? SnakeToDash<K> :
-    P extends 'SnakeToPascal' ? SnakeToPascal<K> :
-    P extends 'SnakeToUpperDash' ? SnakeToUpperDash<K> :
-    P extends 'SnakeToUpperSnake' ? SnakeToUpperSnake<K> :
-    P extends 'UpperDashToCamel' ? UpperDashToCamel<K> :
-    P extends 'UpperDashToDash' ? UpperDashToDash<K> :
-    P extends 'UpperDashToPascal' ? UpperDashToPascal<K> :
-    P extends 'UpperDashToSnake' ? UpperDashToSnake<K> :
-    P extends 'UpperDashToUpperSnake' ? UpperDashToUpperSnake<K> :
-    P extends 'UpperSnakeToCamel' ? UpperSnakeToCamel<K> :
-    P extends 'UpperSnakeToDash' ? UpperSnakeToDash<K> :
-    P extends 'UpperSnakeToPascal' ? UpperSnakeToPascal<K> :
-    P extends 'UpperSnakeToSnake' ? UpperSnakeToSnake<K> :
-    P extends 'UpperSnakeToUpperDash' ? UpperSnakeToUpperDash<K> :
-    never
-  ]: T[K]
-}>;
+export type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
+export type Result<T, P> =
+  T extends string ? string :
+  T extends Array<unknown> ? {
+    [K in keyof T]: Result<T[K], P>
+  }:
+  T extends object ? Prettify<{
+    [K in keyof T as
+      P extends 'CamelToDash' ? CamelToDash<K & string> :
+      P extends 'CamelToPascal' ? CamelToPascal<K & string> :
+      P extends 'CamelToSnake' ? CamelToSnake<K & string> :
+      P extends 'CamelToUpperDash' ? CamelToUpperDash<K & string> :
+      P extends 'CamelToUpperSnake' ? CamelToUpperSnake<K & string> :
+      P extends 'DashToCamel' ? DashToCamel<K & string> :
+      P extends 'DashToPascal' ? DashToPascal<K & string> :
+      P extends 'DashToSnake' ? DashToSnake<K & string> :
+      P extends 'DashToUpperDash' ? DashToUpperDash<K & string> :
+      P extends 'DashToUpperSnake' ? DashToUpperSnake<K & string> :
+      P extends 'PascalToCamel' ? PascalToCamel<K & string> :
+      P extends 'PascalToDash' ? PascalToDash<K & string> :
+      P extends 'PascalToSnake' ? PascalToSnake<K & string> :
+      P extends 'PascalToUpperDash' ? PascalToUpperDash<K & string> :
+      P extends 'PascalToUpperSnake' ? PascalToUpperSnake<K & string> :
+      P extends 'SnakeToCamel' ? SnakeToCamel<K & string> :
+      P extends 'SnakeToDash' ? SnakeToDash<K & string> :
+      P extends 'SnakeToPascal' ? SnakeToPascal<K & string> :
+      P extends 'SnakeToUpperDash' ? SnakeToUpperDash<K & string> :
+      P extends 'SnakeToUpperSnake' ? SnakeToUpperSnake<K & string> :
+      P extends 'UpperDashToCamel' ? UpperDashToCamel<K & string> :
+      P extends 'UpperDashToDash' ? UpperDashToDash<K & string> :
+      P extends 'UpperDashToPascal' ? UpperDashToPascal<K & string> :
+      P extends 'UpperDashToSnake' ? UpperDashToSnake<K & string> :
+      P extends 'UpperDashToUpperSnake' ? UpperDashToUpperSnake<K & string> :
+      P extends 'UpperSnakeToCamel' ? UpperSnakeToCamel<K & string> :
+      P extends 'UpperSnakeToDash' ? UpperSnakeToDash<K & string> :
+      P extends 'UpperSnakeToPascal' ? UpperSnakeToPascal<K & string> :
+      P extends 'UpperSnakeToSnake' ? UpperSnakeToSnake<K & string> :
+      P extends 'UpperSnakeToUpperDash' ? UpperSnakeToUpperDash<K & string> :
+      never
+    ]: Result<T[K], P>
+  }> :
+  T
+;
